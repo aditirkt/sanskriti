a4a1f0295f5f1ab5e7911c03242b710a5baabe3c
manasiChoughule <mchoughule96@gmail.com>
2025-01-20 00:13:22 -0500

    fix rules issue

--- src/app/pages/AllRules.js
+++ src/app/pages/AllRules.js
@@ -1,13 +1,19 @@
 import React from "react";
-import NavigationBar from "../components/NavigationBar";
 import Rules from "../components/Rules";
 import Footer from "../components/Footer";
+import Chess from "./Carrom";
 
 export default function AllRules() {
+  const goBack = () => {
+    window.location.href = "/sanskriti"; // Redirects directly to the Sanskriti page
+  };
   return (
     <>
-      <NavigationBar />
+      <button className="btn button-custom btn-lg" onClick={goBack}>
+        Go Back
+      </button>
       <Rules />
+      <Chess></Chess>
       <Footer />
     </>
   );
