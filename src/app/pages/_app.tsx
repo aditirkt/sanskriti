12f19097b0e17d075f00e3b7ad2f30c07cd693be
manasiChoughule <mchoughule96@gmail.com>
2025-01-24 23:10:50 -0500

    redirect route

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,11 +1,32 @@
-// pages/_app.tsx
 import { AppProps } from "next/app"; // Import AppProps type for typing the component
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "../components/css/index.css"; // Import global CSS
 import "animate.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Detect duplicate "sanskriti/sanskriti" in the URL
+    if (typeof window !== "undefined") {
+      const currentUrl = window.location.href;
+
+      if (currentUrl.includes("/sanskriti/sanskriti")) {
+        // Replace duplicate "sanskriti" with a single one
+        const correctedUrl = currentUrl.replace(
+          "/sanskriti/sanskriti",
+          "/sanskriti"
+        );
+
+        // Redirect to the corrected URL
+        window.location.replace(correctedUrl);
+      }
+    }
+  }, [router]);
+
   return <Component {...pageProps} />;
 }
 
