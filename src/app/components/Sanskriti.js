656742737a249c35afc830a4e65510be69b8f56a
aditirkt <aditi.rktdallas@gmail.com>
2025-01-27 15:35:42 -0600

    error fix

--- src/app/components/Sanskriti.js
+++ src/app/components/Sanskriti.js
@@ -13,12 +13,15 @@ const Sanskriti = () => {
         <p className="content">
           Join us in celebrating the vibrant talents of our community at the
           Sanskriti Awards Ceremony on February 2nd at 6 PM at the Radha Krishna
-          Temple of Dallas! This grand event, honoring the winners of the
-          cultural competition held from January 24th to 26th, promises an
-          unforgettable evening of joy and inspiration. Bring your family and
-          friends to witness the culmination of creativity and dedication
-          showcased across various art, music, dance, and literary categories.
-          Let's come together to applaud the hard work and talent of all
+          Temple of Dallas!
+          <p>
+            This grand event, honoring the winners of the cultural competition
+            held from January 24th to 26th, promises an unforgettable evening of
+            joy and inspiration. Bring your family and friends to witness the
+            culmination of creativity and dedication showcased across various
+            art, music, dance, and literary categories.
+          </p>
+          Let&apos;s come together to applaud the hard work and talent of all
           participants, while creating cherished memories in the spirit of
           culture and togetherness.&nbsp;{" "}
         </p>
